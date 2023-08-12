4866d729ce60bd7d16be42671690a6fe35834c9c
Pankaj Upreti <pankaj.upreti@ukg.com>
2023-08-13 00:30:20 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,35 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  buy1Price: number = 0;
-  qty1: number = 0;
-  buy2Price: number = 0;
-  qty2: number = 0;
-  averagePrice: number = 0;
-  totalAmountInvested: number = 0;
+  buy1Price: number | undefined;
+  qty1: number | undefined;
+  buy2Price: number | undefined;
+  qty2: number | undefined;
+  averagePrice: number | undefined;
+  totalAmountInvested: number | undefined;
 
-  initialPrice: number =0;
-  currentPrice: number =0;
-  percentageMove: number =0;
+  initialPrice: number | undefined;
+  currentPrice: number | undefined;
+  percentageMove: number | undefined;
 
   calculate() {
-    const sumOfPrices = this.buy1Price * this.qty1 + this.buy2Price * this.qty2;
-    const totalQuantity = this.qty1 + this.qty2;
-  
-    const calculatedAveragePrice = sumOfPrices / totalQuantity;
-    this.averagePrice = Number(calculatedAveragePrice.toFixed(2)); // Calculate and round to 2 decimal places
-  
-    this.totalAmountInvested = sumOfPrices;
+    if (this.buy1Price !== undefined && this.qty1 !== undefined && this.buy2Price !== undefined && this.qty2 !== undefined) {
+      const sumOfPrices = this.buy1Price * this.qty1 + this.buy2Price * this.qty2;
+      const totalQuantity = this.qty1 + this.qty2;
 
-  }
+      const calculatedAveragePrice = sumOfPrices / totalQuantity;
+      this.averagePrice = Number(calculatedAveragePrice.toFixed(2)); // Calculate and round to 2 decimal places
 
-  calculatePercentageMove() {
-    if (this.initialPrice && this.currentPrice) {
-      this.percentageMove = ((this.currentPrice - this.initialPrice) / this.initialPrice) * 100;
+      this.totalAmountInvested = sumOfPrices;
     }
   }
 
-
-
-
+  calculatePercentageMove() {
+    if (this.initialPrice !== undefined && this.currentPrice !== undefined) {
+      const percentageMove = ((this.currentPrice - this.initialPrice) / this.initialPrice) * 100;
+      this.percentageMove = parseFloat(percentageMove.toFixed(2));
+    }
+  }
+  
 }
